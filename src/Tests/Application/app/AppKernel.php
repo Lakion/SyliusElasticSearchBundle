6928359cdeaed896c6dc11ebf6bb44317cab1645
Arminek <arkadiusz.k.e@gmail.com>
2016-12-14 17:03:54 +0100

    [ElasticSearch] Init bundle

---
+++ src/Tests/Application/app/AppKernel.php
@@ -0,0 +1,32 @@
+<?php
+
+use Sylius\Bundle\CoreBundle\Application\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+final class AppKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return array_merge([
+            new \Lakion\SyliusElasticSearchBundle\LakionSyliusElasticSearchBundle(),
+        ], parent::registerBundles(), [
+            new \Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
+            new \Sylius\Bundle\ShopBundle\SyliusShopBundle(),
+
+            new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
+            new \Sylius\Bundle\ApiBundle\SyliusApiBundle(),
+            new \FOS\ElasticaBundle\FOSElasticaBundle(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config.yml');
+    }
+}
